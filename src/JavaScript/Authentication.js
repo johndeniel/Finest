ebc78907e1966126834a07fc2cdd63f9977d2382
John Deniel <johndeniel.d37@gmail.com>
2024-02-13 19:45:38 +0800

    Authentication

--- src/JavaScript/Authentication.js
+++ src/JavaScript/Authentication.js
@@ -1,8 +1,12 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth' 
+import {auth} from '../Utils/Database/FirebaseInitialization'
+
 // Google Sign-In button
 const signInButton = document.getElementById('google-login-button')
 signInButton.addEventListener('click', handleGoogleSignInResult)
 
 // Define the handleGoogleSignInResult function
 function handleGoogleSignInResult() {
-  alert('Initialization')
+  const provider = new GoogleAuthProvider()
+  return signInWithPopup(auth, provider)
 }
