4b9d83e008f56dfaec4ef0c862e66cc259007497
johndeniel <johndeniel.d37@gmail.com>
2024-03-21 18:17:19 +0800

    route:2

--- src/app.js
+++ src/app.js
@@ -3,29 +3,49 @@ import renderSwipe from './views/swipe_view'
 import renderGallery from './views/gallery_view'
 import renderAbout from './views/about_view'
 import error404 from './views/error_view'
-import { parseRequestUrl } from './utils/utils'
 
 const routes = {
   '/': renderSwipe,
   '/auth': renderAuth,
   '/gallery': renderGallery,
-  '/about': renderAbout
-
+  '/about': renderAbout,
 }
+
 const router = async () => {
-  //showLoading()
   const request = parseRequestUrl()
   const parseUrl =
     (request.resource ? `/${request.resource}` : '/') +
     (request.id ? '/:id' : '') +
     (request.verb ? `/${request.verb}` : '')
   console.log(request)
+
   const screen = routes[parseUrl] ? routes[parseUrl] : error404
 
   const app = document.getElementById('app')
   app.innerHTML = await screen.render()
+
   if (screen.after_render) await screen.after_render()
-  //hideLoading()
 }
+
+const parseRequestUrl = () => {
+  const address = document.location.hash.slice(1).split('?')[0]
+  const queryString =
+    document.location.hash.slice(1).split('?').length === 2
+      ? document.location.hash.slice(1).split('?')[1]
+      : ''
+
+  const url = address.toLowerCase() || '/'
+  const r = url.split('/')
+  const q = queryString.split('=')
+  return {
+    resource: r[1],
+    id: r[2],
+    verb: r[3],
+    name: q[0],
+    value: q[1],
+  }
+}
+
+
 window.addEventListener('load', router)
 window.addEventListener('hashchange', router)
